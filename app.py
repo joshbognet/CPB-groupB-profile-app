cf5a773469ab29eed826caaafbe903a10e873b62
EbubeOkodike <ebubedikehh@gmail.com>
2022-09-17 23:16:17 +0100

    I HAVE DONE ITgit status!

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -29,30 +30,74 @@ class Student(db.Model):
 @app.route('/')
 def Home():
     students=Student.query.all()
-    return render_template('index.html', s=students)
+    return render_template('index.html', students = students)
 
-@app.route('/create', methods=['GET','POST'])
+@app.route('/Create', methods=['GET', 'POST'])
 def Create():
     if request.method=="POST":
-        fname= request.form['firstname']
-        lname= request.form['lastname']
-        email= request.form['email']
-        age= int(request.form['age'])
-        bio=request.form['bio']
+        fname = request.form['firstname']
+        lname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio =request.form['bio']
 
         student= Student(
-        firstname= fname,
-        lastname= lname,
-        email= email,
-        age= age,
-        bio=bio
-    
+        firstname = fname,
+        lastname = lname,
+        email = email,
+        age = age,
+        bio = bio
         )
+        
         db.session.add(student)
         db.session.commit()
         return redirect(url_for('Home'))
-    
+
     return render_template('create.html')
+
+@app.route('/<int:student_id>/')
+def student(student_id):
+    student = Student.query.get_or_404(student_id)
+    return render_template('student.html', student = student)
+
+
+@app.route('/<int:student_id>/update', methods = ('GET', 'POST'))
+def Update(student_id):
+    student = Student.query.get_or_404(student_id)
+
+    if request.method=="POST":
+        fname = request.form['firstname']
+        lname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio =request.form['bio']
+        
+        firstname = fname,
+        lastname = lname,
+        email = email,
+        age = age,
+        bio = bio
+
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for('Home'))
+
+    return render_template('update.html', student = student)
+
+@app.post('/<int:student_id>/delete')
+def Delete(student_id):
+    student = Student.query.get_or_404(student_id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('Home'))
+
+
+
+        
+        
+
+
+
 
 
 
